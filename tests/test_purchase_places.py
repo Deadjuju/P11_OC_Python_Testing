1d75a8b87c3a46dd24eb02a6737c00160fcbea54
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-11 17:57:11 +0200

    feat(tests): add test for limit places per club

--- tests/test_purchase_places.py
+++ tests/test_purchase_places.py
@@ -1,23 +1,24 @@
-from tests.conftest import client
+import server
+from tests.conftest import client, mocker_clubs, mocker_competitions
 
-mocker_club = {
-    'name': 'Simply Lift',
-    'email': 'john@simplylift.co',
-    'points': '13'
-}
 
-mocker_competition = {
+club_name = "Simply Lift"
+
+competition = {
     "name": "Spring Festival",
     "date": "2020-03-27 10:00:00",
     "numberOfPlaces": "25"
 }
 
 
-def test_places_required_greater_than_points_club(client):
+def test_places_required_greater_than_points_club(client, mocker):
+
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
 
     data = {
-        'club': mocker_club['name'],
-        'competition': mocker_competition['name'],
+        'club': club_name,
+        'competition': competition['name'],
         'places': 1000,
     }
     response = client.post('/purchasePlaces', data=data)
@@ -26,14 +27,36 @@ def test_places_required_greater_than_points_club(client):
     assert b'The club does not have enough points.' in response.data
 
 
-def test_places_successfully_buyed(client):
+def test_limits_of_places_per_club_exceeded(client, mocker):
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
 
     data = {
-        'club': mocker_club['name'],
-        'competition': mocker_competition['name'],
+        'club': club_name,
+        'competition': competition['name'],
+        'places': 13,
+    }
+    response = client.post('/purchasePlaces', data=data)
+
+    assert response.status_code == 200
+    assert b'You cannot buy more than 12 places per competition.' in response.data
+
+
+def test_places_successfully_buyed(client, mocker):
+
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+
+    data = {
+        'club': club_name,
+        'competition': competition['name'],
         'places': 2,
     }
     response = client.post('/purchasePlaces', data=data)
 
+    places_required = int(data['places'])
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+
+    assert competition['numberOfPlaces'] == 23
     assert response.status_code == 200
     assert b'Great-booking complete!' in response.data
