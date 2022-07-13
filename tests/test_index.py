50d04f46fa4c018979d9f7f585ee3c7fd85b44d3
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-13 10:18:46 +0200

    feat(test_index): check the name of template used

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,7 +1,15 @@
-from tests.conftest import client
+from tests.conftest import client, captured_templates
 
 
 def test_should_status_code_ok(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Welcome to the GUDLFT Registration Portal!' in response.data
+
+
+def test_template_index(client, captured_templates):
+    expected_template_name = "index.html"
+    response = client.get('/')
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
