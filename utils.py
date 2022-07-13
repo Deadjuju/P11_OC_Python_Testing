a161dd83241d11d714a7b01111c9542a0760ee97
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-13 15:11:22 +0200

    feat(utils): add function to find a club in list

--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 from datetime import datetime
 
 
+class ClubNotFoundError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+def get_club_by_key(club_list: list[dict], club_info: str, key: str) -> dict:
+    """
+    Find a club
+    """
+    for club in club_list:
+        if club[key] == club_info:
+            return club
+    message = f"No correspondence with this {key}."
+    raise ClubNotFoundError(message)
+
+
 def is_date_not_already_past(date: str) -> bool:
     """determines if a date has not yet passed
 
