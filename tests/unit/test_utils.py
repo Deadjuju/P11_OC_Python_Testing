3358a316b74da6c179585a8b6e2f3b88d08d4d1e
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-15 17:58:31 +0200

    test(unit tests): add utils tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timedelta
 
 import pytest
-from utils import (get_club_by_key,
+from utils import (check_places_number_for_a_competition_and_update,
+                   get_club_by_key,
                    get_competition,
+                   init_a_club_if_not_in_dict,
                    is_date_not_already_past,
                    update_points_or_places,
                    ClubNotFoundError,
@@ -14,11 +16,19 @@ tomorrow = (datetime.now() + timedelta(1)).strftime("%Y-%m-%d %H:%M:%S")
 
 
 def test_should_false_for_date_in_past():
+    """
+    Return FALSE for a past date
+    """
+
     date_in_past = "2020-03-27 10:00:00"
     assert is_date_not_already_past(date_in_past) == False
 
 
 def test_should_true_for_date_in_future():
+    """
+    Return TRUE for a future date
+    """
+
     date_in_future = tomorrow
     assert is_date_not_already_past(date_in_future) == True
 
@@ -82,3 +92,81 @@ def test_success_to_update_points_or_places():
 def test_fail_to_update_points_with_negative_result():
     with pytest.raises(NegativeResultError):
         update_points_or_places(places_required=4, current_points_or_places=3)
+
+
+def test_should_init_a_club():
+    """
+    GIVEN a dictionary whose keys are club names and given a club not present in this dictionary,
+    WHEN the "init_a_club_if_not_in_dict" function is apply,
+    THEN the club is added to the dictionary and has the value of an empty dictionary.
+    """
+
+    club_places_per_competition = club_places_per_competition = {
+        'Simply Lift': {"Super Smash Force": 1, "Fall Classic": 12}
+    }
+    club_name = "Lift Ting"
+    expected_result = {
+        'Simply Lift': {"Super Smash Force": 1, "Fall Classic": 12},
+        'Lift Ting': {}
+    }
+    init_a_club_if_not_in_dict(club_places_per_competition, club_name)
+    assert club_places_per_competition == expected_result
+
+
+def test_already_in_dict_when_try_init_a_club():
+    """
+    GIVEN a dictionary whose keys are club names and given a club already present in this dictionary,
+    WHEN the "init_a_club_if_not_in_dict" function is apply,
+    THEN the dictionary remains unchanged.
+    """
+
+    club_places_per_competition = club_places_per_competition = {
+        'Simply Lift': {"Super Smash Force": 1, "Fall Classic": 12}
+    }
+    club_name = "Simply Lift"
+    expected_result = {
+        'Simply Lift': {"Super Smash Force": 1, "Fall Classic": 12}
+    }
+    init_a_club_if_not_in_dict(club_places_per_competition, club_name)
+    assert club_places_per_competition == expected_result
+
+
+def test_should_true_total_places_for_a_competition_less_or_equal_than_twelve():
+    """
+    GIVEN sum of current_places_competition and required_places
+    WHEN the sum <= 12
+    THEN return True
+    """
+
+    club_places_per_competition = {
+        'Simply Lift': {"Super Smash Force": 3, "Fall Classic": 12}
+    }
+    club_name = "Simply Lift"
+    competition_name = "Super Smash Force"
+    required_places = 5
+    result = check_places_number_for_a_competition_and_update(club_places_per_competition,
+                                                            club_name,
+                                                            competition_name,
+                                                            required_places)
+    assert result == True
+
+
+def test_should_false_total_places_for_a_competition_greater_than_twelve():
+    """
+    GIVEN sum of current_places_competition and required_places
+    WHEN the sum > 12
+    THEN return False
+    """
+
+    club_places_per_competition = {
+        'Simply Lift': {"Super Smash Force": 6, "Fall Classic": 12}
+    }
+    club_name = "Simply Lift"
+    competition_name = "Super Smash Force"
+    required_places = 7
+    # 6 + 7 = 13 > 12
+    result = check_places_number_for_a_competition_and_update(club_places_per_competition,
+                                                              club_name,
+                                                              competition_name,
+                                                              required_places)
+    assert result == False
