891625c32c6bb2f29358024bf65010d2bf229c30
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-16 23:55:12 +0200

    test(logout): add tests

---
+++ tests/unit/test_logout.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+from tests.conftest import client, captured_templates
+
+
+def test_should_redirect(client, captured_templates):
+    """
+    When user logout, he is automatically redirected.
+    """
+
+    response = client.get('/logout')
+    assert response.status_code == HTTPStatus.FOUND
+
+
+def test_should_redirect_to_index(client, captured_templates):
+    """
+    After logout, user redirected to index page.
+    """
+
+    response = client.get('/logout', follow_redirects=True)
+    assert response.status_code == HTTPStatus.OK
+    assert b"Welcome to the GUDLFT Registration Portal!" in response.data
+
+    expected_template_name = "index.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
