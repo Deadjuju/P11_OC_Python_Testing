7851370d8193ce3a047247354501d42b9d966fe3
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-16 20:23:09 +0200

    test(purchase places): add tests

--- tests/test_purchase_places.py
+++ tests/test_purchase_places.py
@@ -1,5 +1,9 @@
+from http import HTTPStatus
+
 import server
-from tests.conftest import client, mocker_clubs, mocker_competitions
+from server import PLACES_LIMIT_PER_COMPETITION
+from utils import NegativeResultError
+from tests.conftest import client, mocker_clubs, mocker_competitions, valid_club
 
 
 club_name = "Simply Lift"
@@ -11,52 +15,190 @@ competition = {
 }
 
 
-def test_places_required_greater_than_points_club(client, mocker):
+def test_places_required_greater_than_points_club(client, mocker, valid_club, captured_templates):
+    """
+    GIVEN a VALID club, a Valid competition & LARGE number of places,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "welcome" page with an appropriate message.
+    """
 
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
+    mocker.patch('server.get_club_by_key', return_value=valid_club)
+    very_large_number_of_places = 1000
 
-    data = {
-        'club': club_name,
+    data_to_post = {
+        'club': valid_club['name'],
         'competition': competition['name'],
-        'places': 1000,
+        'places': very_large_number_of_places,
     }
-    response = client.post('/purchasePlaces', data=data)
+    response = client.post('/purchasePlaces', data=data_to_post)
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert b'The club does not have enough points.' in response.data
 
+    expected_template_name = "welcome.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
 
-def test_limits_of_places_per_club_exceeded(client, mocker):
+
+def test_places_requested_exceed_places_per_competition(client, mocker, valid_club, captured_templates):
+    """
+    GIVEN a VALID club, a Valid competition & number of places > limit per competition,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "booking" page with an appropriate message.
+    """
+
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
+    mocker.patch('server.get_club_by_key', return_value=valid_club)
 
-    data = {
+    places_required = PLACES_LIMIT_PER_COMPETITION + 1
+
+    data_to_post = {
         'club': club_name,
         'competition': competition['name'],
-        'places': 13,
+        'places': places_required,
     }
-    response = client.post('/purchasePlaces', data=data)
+    response = client.post('/purchasePlaces', data=data_to_post)
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert b'You cannot buy more than 12 places per competition.' in response.data
 
+    expected_template_name = "booking.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
 
-def test_places_successfully_buyed(client, mocker):
+
+def test_not_enough_places_available(client, mocker, valid_club, captured_templates):
+    """
+    GIVEN a VALID club, a Valid competition & places available < required places,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "booking" page with an appropriate message.
+    """
 
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
+    mocker.patch('server.get_club_by_key', return_value=valid_club)
+    mocker.patch('server.get_competition', return_value=competition)
 
-    data = {
+    competition["numberOfPlaces"] = "1"
+    places_required = int(competition['numberOfPlaces']) + 1
+    data_to_post = {
         'club': club_name,
         'competition': competition['name'],
-        'places': 2,
+        'places': places_required,
     }
-    response = client.post('/purchasePlaces', data=data)
 
-    places_required = int(data['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+    with mocker.patch('server.update_points_or_places',
+                      side_effect=NegativeResultError("Negative values are not allowed")):
+        response = client.post('/purchasePlaces', data=data_to_post)
 
-    assert competition['numberOfPlaces'] == 23
-    assert response.status_code == 200
-    assert b'Great-booking complete!' in response.data
+        assert response.status_code == HTTPStatus.OK
+        assert b'This competition does not have as many places available.' in response.data
+
+        expected_template_name = "booking.html"
+        template, context = captured_templates[0]
+        assert len(captured_templates) == 1
+        assert template.name == expected_template_name
+
+
+# def test_not_enough_places_available(client, mocker, valid_club, captured_templates):
+#     """
+#     GIVEN a VALID club, a Valid competition & places available < required places,
+#     WHEN the user tries to access the view route "book()",
+#     THEN the user arrives on the "booking" page with an appropriate message.
+#     """
+#
+#     mocker.patch.object(server, 'clubs', mocker_clubs)
+#     mocker.patch.object(server, 'competitions', mocker_competitions)
+#     mocker.patch('server.get_club_by_key', return_value=valid_club)
+#     competition = {
+#         "name": "Spring Festival",
+#         "date": "2020-03-27 10:00:00",
+#         "numberOfPlaces": "1"
+#     }
+#     mocker.patch('server.get_competition', return_value=competition)
+#     # mocker.patch('server.update_points_or_places', return_value=(NegativeResultError))
+#
+#     places_required = int(competition['numberOfPlaces']) + 1
+#     data = {
+#         'club': club_name,
+#         'competition': competition['name'],
+#         'places': places_required,
+#     }
+#     response = client.post('/purchasePlaces', data=data)
+#
+#     assert response.status_code == HTTPStatus.OK
+#     assert b'This competition does not have as many places available.' in response.data
+#
+#     expected_template_name = "booking.html"
+#     template, context = captured_templates[0]
+#     assert len(captured_templates) == 1
+#     assert template.name == expected_template_name
+
+
+def test_not_enough_places_available(client, mocker, valid_club, captured_templates):
+    """
+    GIVEN a VALID club, a Valid competition, places available > required places
+    & total number of places requested exceeds the authorized limit,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "booking" page with an appropriate message.
+    """
+
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+    mocker.patch('server.get_club_by_key', return_value=valid_club)
+    mocker.patch('server.get_competition', return_value=competition)
+    mocker.patch('server.update_points_or_places', return_value=15)
+    mocker.patch('server.check_places_number_for_a_competition_and_update', return_value=False)
+
+    places_required = 10
+    data_to_post = {
+        'club': club_name,
+        'competition': competition['name'],
+        'places': places_required,
+    }
+    response = client.post('/purchasePlaces', data=data_to_post)
+    expected_phrase = f'You cannot buy more than {PLACES_LIMIT_PER_COMPETITION} places per competition.'
+    assert response.status_code == HTTPStatus.OK
+    assert expected_phrase in response.data.decode()
+
+    expected_template_name = "booking.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
+
+
+def test_places_successfully_buyed(client, mocker, valid_club, captured_templates):
+    """
+    GIVEN a VALID club, a Valid competition, places available > required places
+    & total number of places requested < the authorized limit,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "welcome" page with an appropriate message:
+     -> "Great-booking complete!".
+    """
+
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+    mocker.patch('server.get_club_by_key', return_value=valid_club)
+    mocker.patch('server.get_competition', return_value=competition)
+    mocker.patch('server.update_points_or_places', return_value=15)
+    mocker.patch('server.check_places_number_for_a_competition_and_update', return_value=True)
+
+    places_required = 10
+    data_to_post = {
+        'club': club_name,
+        'competition': competition['name'],
+        'places': places_required,
+    }
+    response = client.post('/purchasePlaces', data=data_to_post)
+    assert response.status_code == HTTPStatus.OK
+    assert b"Great-booking complete!" in response.data
+
+    expected_template_name = "welcome.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
