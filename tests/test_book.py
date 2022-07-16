7bdc8e23e60dc147faf1540f8d8cd31ad0c4fc81
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-16 12:35:05 +0200

    test(book): make tests for book view

--- tests/test_book.py
+++ tests/test_book.py
@@ -1,19 +1,29 @@
+from http import HTTPStatus
+
 import server
-from tests.conftest import client, captured_templates, mocker_clubs, mocker_competitions
+from tests.conftest import (captured_templates,
+                            client,
+                            future_competition,
+                            invalid_club,
+                            mocker_clubs,
+                            mocker_competitions,
+                            past_competition,
+                            valid_club)
 
 
-club = mocker_clubs[0]
-passed_competition = mocker_competitions[0]
-futur_competition = mocker_competitions[2]
+def test_book_for_passed_competition(client, mocker, valid_club, past_competition, captured_templates):
+    """
+    GIVEN a VALID club & PAST competition,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "welcome" page with an appropriate message.
+    """
 
-
-def test_book_for_passed_competition(client, mocker, captured_templates):
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
 
-    response = client.get(f"/book/{passed_competition['name']}/{club['name']}")
+    response = client.get(f"/book/{past_competition['name']}/{valid_club['name']}")
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert b"This event has already passed." in response.data
 
     expected_template_name = "welcome.html"
@@ -21,14 +31,20 @@ def test_book_for_passed_competition(client, mocker, captured_templates):
     assert len(captured_templates) == 1
     assert template.name == expected_template_name
 
-def test_book_with_invalid_club(client, mocker, captured_templates):
+
+def test_book_with_invalid_club(client, mocker, invalid_club, future_competition, captured_templates):
+    """
+    GIVEN an INVALID club & FUTURE competition,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "index" page with an appropriate message.
+    """
 
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
 
-    response = client.get(f"/book/{futur_competition['name']}/{club['name']}XXX")
+    response = client.get(f"/book/{future_competition['name']}/{invalid_club['name']}")
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert b"You don&#39;t exist, sorry." in response.data
 
     expected_template_name = "index.html"
@@ -38,14 +54,21 @@ def test_book_with_invalid_club(client, mocker, captured_templates):
 
 
 
-def test_book_with_invalid_competition(client, mocker, captured_templates):
+def test_book_with_invalid_competition(client, mocker, valid_club, future_competition, captured_templates):
+    """
+    GIVEN a VALID club & FUTRURE competition,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "index" page with an appropriate message.
+    """
+
+    invalid_competition_name = f"XXXX{future_competition['name']}XXXX"
 
     mocker.patch.object(server, 'clubs', mocker_clubs)
     mocker.patch.object(server, 'competitions', mocker_competitions)
 
-    response = client.get(f"/book/{futur_competition['name']}XXXX/{club['name']}")
+    response = client.get(f"/book/{invalid_competition_name}/{valid_club['name']}")
 
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK
     assert b"This competition does not exist." in response.data
 
     expected_template_name = "index.html"
@@ -53,3 +76,26 @@ def test_book_with_invalid_competition(client, mocker, captured_templates):
     assert len(captured_templates) == 1
     assert template.name == expected_template_name
 
+
+def test_book_with_valid_club_and_valid_future_competition(client,
+                                                           mocker,
+                                                           valid_club,
+                                                           future_competition,
+                                                           captured_templates):
+    """
+    GIVEN a VALID club & FUTURE competition,
+    WHEN the user tries to access the view route "book()",
+    THEN the user arrives on the "booking" page.
+    """
+
+    mocker.patch.object(server, 'clubs', mocker_clubs)
+    mocker.patch.object(server, 'competitions', mocker_competitions)
+
+    response = client.get(f"/book/{future_competition['name']}/{valid_club['name']}")
+
+    assert response.status_code == HTTPStatus.OK
+
+    expected_template_name = "booking.html"
+    template, context = captured_templates[0]
+    assert len(captured_templates) == 1
+    assert template.name == expected_template_name
